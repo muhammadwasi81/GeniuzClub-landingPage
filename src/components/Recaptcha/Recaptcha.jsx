f95d17882f530392205d53885cfd6c7fd1af5ebb
muhammadwasi81 <wasiarain819@gmail.com>
2022-10-24 21:46:51 +0500

    Integrate: Email API

--- src/components/Recaptcha/Recaptcha.jsx
+++ src/components/Recaptcha/Recaptcha.jsx
@@ -1,95 +1,99 @@
+import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useCallback, useEffect, useState } from 'react'
-import { useGoogleReCaptcha } from 'react-google-recaptcha-v3'
-import $ from 'jquery'
 
-// const SITE_KEY = '6Lev4X8iAAAAAF9UgTDVOib5O6QkQN7uuvD3u53z';
+const SITE_KEY = '6LfkUIYiAAAAAI-6GH-AZrG-wOBXIIEiZT5TcTFn'
 
 function Recaptcha() {
   const [loading, setLoading] = useState(false)
-  const { executeRecaptcha } = useGoogleReCaptcha()
-  const [token, setToken] = useState('')
   const [email, setEmail] = useState('')
-
-  const registerUser = useCallback(async () => {
-    if (!executeRecaptcha) {
-      return
-    }
-    console.log('token', token)
-    const result = await executeRecaptcha('register')
-    setToken(result)
-    setLoading(true)
-    setEmail('')
-  }, [executeRecaptcha])
-
-  const handleTextChange = useCallback((event) => {
-    setEmail(event.target.value)
-  }, [])
+  const [response, setResponse] = useState(null)
 
   useEffect(() => {
-    if (!executeRecaptcha) {
-      return
-    }
-    const handleReCaptchaVerify = async () => {
-      const token = await executeRecaptcha('register')
-      setToken(token)
-    }
-    handleReCaptchaVerify()
-  }, [executeRecaptcha])
+    const loadScriptByURL = (id, url, callback) => {
+      const isScriptExist = document.getElementById(id)
 
-  function sendReq() {
+      if (!isScriptExist) {
+        var script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.src = url
+        script.id = id
+        script.onload = function () {
+          if (callback) callback()
+        }
+        document.body.appendChild(script)
+      }
+      if (isScriptExist && callback) callback()
+    }
+
+    loadScriptByURL(
+      'recaptcha-key',
+      `https://www.google.com/recaptcha/api.js?render=${SITE_KEY}`,
+      function () {
+        console.log('Script loaded!')
+      }
+    )
+  }, [])
+
+  const handleOnClick = (e) => {
+    e.preventDefault()
+    setLoading(true)
     window.grecaptcha.ready(() => {
       window.grecaptcha
-        .execute('6LfkUIYiAAAAAI-6GH-AZrG-wOBXIIEiZT5TcTFn', {
-          action: 'homepage',
+        .execute(SITE_KEY, {
+          action: 'submit',
         })
         .then((token) => {
-          callApi(token)
+          console.log(token, 'token')
+          handleSubmit(token)
         })
     })
   }
 
-  function callApi(recaptchaToken) {
-    let email = document.getElementById('f-email').value
-    let json = {
+  const handleSubmit = async (token) => {
+    const payload = {
       email: email,
     }
-
-    $.ajax({
-      type: 'POST',
-      url: 'https://api.geniuz.club/landingpage/marketing/WhiteList',
-      crossDomain: true,
-      data: JSON.stringify(json),
-      contentType: 'application/json',
-      dataType: 'json',
-      beforeSend: function (xhr) {
-        xhr.setRequestHeader('recaptchaToken', recaptchaToken)
-        xhr.setRequestHeader('Api-Version', '1.0')
-      },
-      success: function (data) {
-        alert('Request done!')
-      },
-      error: function (xhr, status, error) {
-        alert('Error!')
-      },
-    })
+    try {
+      setLoading(true)
+      const response = await axios.post(
+        `https://api.geniuz.club/landingpage/marketing/WhiteList`,
+        payload,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Api-Version': '1.0',
+            recaptchaToken: token,
+          },
+        }
+      )
+      setResponse(response)
+      setLoading(false)
+      setEmail('')
+      console.log(response.data, 'response')
+    } catch (error) {
+      setLoading(false)
+      console.log(error, 'error')
+      throw new Error('Something went wrong', { cause: error })
+    }
   }
   return (
     <>
       <div className="d-flex justify-content-center flex-column">
         <input
-          id="f-email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           type="email"
           name="email"
           placeholder="Enter your email"
           className="form-control"
         />
         <button
-          onClick={sendReq}
+          onClick={handleOnClick}
           style={{ margin: 'auto' }}
           className="btn btn-warning my-4 w-25"
+          disabled={loading}
         >
-          Send
+          {loading ? 'Submitting...' : 'Submit'}
         </button>
       </div>
     </>
